d1967842df7768fb3472ef51430d4c99f5f7cd51
nurseyfullazade <aseyfullazade@std.beu.edu.az>
2024-01-21 19:52:16 +0400

    Commit3

--- Airline/FlightTBL.cs
+++ Airline/FlightTBL.cs
@@ -40,25 +40,17 @@ namespace Airline
             {
                 try
                 {
-                    // Open the database connection
                     Con.Open();
-
-                    // SQL query to insert flight information into the database
                     string query = "insert into FlightTbl values('" + FcodeTb.Text + "','" + Fsrc.SelectedItem.ToString() + "', '" + FDest.SelectedItem.ToString() + "', '" + FDate.Value.ToString() + "', '" + SeatNum.Text + "')";
 
                     // Create a SqlCommand object and execute the query
                     SqlCommand cmd = new SqlCommand(query, Con);
-                    cmd.ExecuteNonQuery();
-
-                    // Display a success message
-                    MessageBox.Show("Flight Recorded Successfully");
-
-                    // Close the database connection
+                    cmd.ExecuteNonQuery();                    
+                    MessageBox.Show("Flight Recorded Successfully");                 
                     Con.Close();
                 }
                 catch (Exception Ex)
                 {
-                    // Display an error message if an exception occurs
                     MessageBox.Show(Ex.Message);
                 }
             }
@@ -78,7 +70,6 @@ namespace Airline
         // Event handler for the "View Flights" button
         private void button3_Click(object sender, EventArgs e)
         {
-            // Open the ViewFlights form and hide the current FlightTbl form
             ViewFlights viewflt = new ViewFlights();
             viewflt.Show();
             this.Hide();
@@ -87,11 +78,10 @@ namespace Airline
         // Event handler for the close button (X) on the form
         private void label8_Click(object sender, EventArgs e)
         {
-            // Exit the application when the close button is clicked
             Application.Exit();
         }
 
-        // Event handler for the "Back to Home" button
+        // Event handler for the "Back" button
         private void button4_Click(object sender, EventArgs e)
         {
             // Open the Home form and hide the current FlightTbl form
